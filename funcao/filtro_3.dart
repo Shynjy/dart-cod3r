5e0fe9437acce5dd847da1a1350297df16d8041c
Shynjy <furia.designer@gmail.com>
2020-09-23 18:09:17 -0300

    update folder funcao

--- funcao/filtro_3.dart
+++ funcao/filtro_3.dart
@@ -1,2 +1,19 @@
-List<tipo> filtrar<tipo>(List<tipo> lista, bool Function(tipo) fn) {}
-main() {}
+List<tipoGenerico> filtrar<tipoGenerico>(
+    List<tipoGenerico> lista, bool Function(tipoGenerico) fn) {
+  List<tipoGenerico> listaFiltrado = [];
+  for (tipoGenerico elemento in lista) {
+    if (fn(elemento)) {
+      listaFiltrado.add(elemento);
+    }
+  }
+  return listaFiltrado;
+}
+
+main() {
+  List<double> notas = [8.2, 7.1, 6.2, 4.4, 3.9, 8.8, 9.1, 5.1];
+
+  bool Function(double) boasNotasFn = (double nota) => nota >= 7.5;
+
+  var somenteNotasBoas = filtrar(notas, boasNotasFn);
+  print(somenteNotasBoas);
+}
